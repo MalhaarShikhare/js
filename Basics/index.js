fbe8e613fa1857446ad9ff16002d291dbc7a6143
Malhaar Shikhare <shikharemalhaar@gmail.com>
2024-06-11 12:19:40 +0530

    Made changes in different branches

--- Basics/index.js
+++ Basics/index.js
@@ -1,1 +1,2 @@
 console.log("Made these changes from cmd using vi after i cloned it to local")
+console.log("Made these changes from a different branch ")
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
+
 # js
 A code repo for all my java script code and learnings
+Small projects from cohort as well.
